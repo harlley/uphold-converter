43ccc79812e53c437d385b93ccaa40e86a66682e
Harlley Oliveira <harlley@gmail.com>
2020-11-29 13:40:29 +0000

    fix items alignment

--- src/components/Conversion.js
+++ src/components/Conversion.js
@@ -6,13 +6,15 @@ const Conversion = ({ amount, rate }) => {
   const currency = rate.pair.slice(3, 6);
   return (
     <Container>
-      <div style={{ marginLeft: '10px'}}>
+      <div style={{ marginLeft: '10px', fontSize: '1.2rem', backgroundColor: '#666', color: 'white', display: 'flex', alignItems: 'center'}}>
         {total}
       </div>
-      <div>
-        <img src={require(`../images/${currency}.png`).default} width="15" height="15" alt="" />
-        <span style={{width: '50px', display: 'inline-block', marginLeft: '5px'}}>{currency}</span>
+      <div style={{backgroundColor: '#666', color: 'white', display: 'flex', alignItems: 'center', minWidth: '100px'}}>
         
+        <img src={require(`../images/${currency}.png`).default} width="25" height="25" alt="" />
+        <span style={{marginLeft: '5px'}}>
+          {currency}
+        </span>
       </div>
     </Container>
   )
@@ -21,6 +23,9 @@ const Conversion = ({ amount, rate }) => {
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
+  padding: 10px;
+  background-color: #eee;
+  margin-bottom: 2px;
 `
 
 export default Conversion;
--- src/components/SelectCurrency.js
+++ src/components/SelectCurrency.js
@@ -1,40 +1,84 @@
-import Select from "react-select";
+import Select, { components } from "react-select";
 import styled from 'styled-components';
 
 export const SelectCurrency = ({ currency, usedCurrencies, onChange, amount, setAmount }) => {
 
+  const FlagIcon = ({value}) => {
+   return (
+    <>
+      <img src={require(`../images/${value}.png`).default} width="25" height="25" alt=""/>
+      <span style={{paddingLeft: 5, backgroundColor: 'red', display: 'flex', alignItems: 'center'}}>{value}</span>
+    </>
+   )
+  }
 
   const options = usedCurrencies.map(val => { 
     return { 
       value: val,
-      label: <div><img src={require(`../images/${val}.png`).default} width="15" height="15" alt="" />{val}</div> 
+      label: <FlagIcon value={val}/>
     };
   });
 
+
+  const DropdownIcon = () => {
+    return (
+      <svg height="5" viewBox="0 0 8 5" width="8" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd" transform="translate(-4 -6)"><path d="m0 0h16v16h-16z"/><path d="m4.50260994 6h6.99478016c.2761423 0 .5.22385763.5.5 0 .13260824-.0526785.2597852-.1464466.35355339l-3.49739011 3.49739011c-.19526215.1952621-.51184463.1952621-.70710678 0l-3.49739006-3.49739011c-.19526215-.19526215-.19526215-.51184463 0-.70710678.09376819-.09376819.22094514-.14644661.35355339-.14644661z" fill="#8494a5"/></g></svg>
+    )
+  };
+  
+  const DropdownIndicator = props => {
+    return (
+      <components.DropdownIndicator {...props}>
+        <DropdownIcon />
+      </components.DropdownIndicator>
+    );
+  };
+
   const colourStyles = {
-    control: styles => ({ ...styles, borderRadius: 99, width: 200 }),
+    control: styles => ({ ...styles, borderRadius: 99, width: 100, height: '100%', border: 'none' }),
+    indicatorSeparator: styles => ({ ...styles, display: 'none' }),
+    singleValue: styles => ({ ...styles, backgroundColor: '#666', display: 'flex', alignItems: 'center', color: 'white', justifyContent: 'center', verticalAlign: 'center' }),
+    option: styles => ({ ...styles, marginBottom: 1, display: 'flex', alignItem: 'center' }),
+    menu: styles => ({ ...styles, marginTop: 2, borderRadius: 6 }),
+
   };
   
 
   return (
     <Container>
-      <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
-
+      <Input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
       <Select
-        defaultValue={ { value: currency, label: <div><img src={require(`../images/${currency}.png`).default} width="15" height="15" alt="" />{currency}</div> }}
+        defaultMenuIsOpen={true}
+        menuIsOpen={true}
+        closeMenuOnSelect={false}
+        closeMenuOnScroll={false}
+        defaultValue={{ value: currency, label: <FlagIcon value={currency}/> }}
         onChange={onChange}
         options={options}
-        styles={colourStyles}        
+        styles={colourStyles}
+        components={{ DropdownIndicator }}
       />
-
     </Container>
   )
 }
 
+const Input = styled.input`
+  background-color: transparent;
+  width: 200px;
+  border: none;
+  color: white;
+  font-size: 2rem;
+  outline: none;
+  padding-left: 5px;
+`
 
 const Container = styled.div`
+  width: 100%;
+  padding: 15px;
   display: flex;
-  background-color: blue;
+  background-color: blue/*rgb(243, 247, 252)*/;
+  justify-content: space-between;
+  border-radius: 12px;
 
 `
 
